364033c948503bc33b5ac456b5bcdaf814e42a48
Patrick Fitzgibbon <patrickfitzgibbon@wustl.edu>
2018-02-19 15:56:07 -0600

    sprint 1 mostly done

---
+++ doughreturn_game/Assets/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	private Rigidbody rb;
+	public bool isGrounded;
+	static public Vector3 reset;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		isGrounded = false;
+		reset = gameObject.transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		//x move
+		if (Input.GetKey(KeyCode.LeftArrow)) rb.AddForce (-15, 0, 0);
+		else if (Input.GetKey(KeyCode.RightArrow)) rb.AddForce (15, 0, 0);
+
+		//y move
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			if (isGrounded) rb.AddForce (0, 400, 0);
+		}
+	}
+
+	void OnCollisionEnter(Collision other) {
+		if (other.gameObject.tag == "ground")
+			isGrounded = true;
+		if (other.gameObject.tag == "death")
+			gameObject.transform.position = reset;
+	}
+
+	void OnCollisionExit(Collision other) {
+		if (other.gameObject.tag == "ground")
+			isGrounded = false;
+	}
+}
