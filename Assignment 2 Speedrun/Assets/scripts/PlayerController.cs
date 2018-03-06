00c7f2d887d1be7ee46e68785786f611584ef777
Ian Boyer-Edwards <boyer-edwards.ian@wustl.edu>
2018-03-05 23:16:31 -0600

    Added more to level

---
+++ Assignment 2 Speedrun/Assets/scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody pMovement;
+
+	private float moveSpeed = 10.0f;
+	private float jumpSpeed = 10.0f;
+	private float moveX;
+	private bool ground;
+
+	// Use this for initialization
+	void Start () {
+		pMovement = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ doughreturn_game/Assets/Scripts/MovingPlatform1.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform1 : MonoBehaviour {
+
+	//private int speed = 4;
+	//private int moveRight = 1;
+	private GameObject target=null;
+	private Vector3 offset;
+
+	public float min=2f;
+	public float max=3f;
+	// Use this for initialization
+	void Start () {
+
+		min=transform.position.x;
+		max=transform.position.x+12;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+
+		transform.position =new Vector3(Mathf.PingPong(Time.time*5,max-min)+min, transform.position.y, transform.position.z);
+
+	}
+
+	// Collision stuff to keep the player on the platform
+	// Not working for now
+	void OnTriggerStay2D(Collider2D col){
+		target = col.gameObject;
+		offset = target.transform.position - transform.position;
+	}
+	void OnTriggerExit2D(Collider2D col){
+		target = null;
+	}
+	void LateUpdate(){
+		if (target != null) {
+			target.transform.position = transform.position+offset;
+		}
+	}
+}
