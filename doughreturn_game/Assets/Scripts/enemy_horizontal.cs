efa531bfe3af3069d219326a19213862c3224974
Ian Boyer-Edwards <boyer-edwards.ian@wustl.edu>
2018-04-16 00:23:49 -0500

    Added enemy movement scripts

---
+++ doughreturn_game/Assets/Scripts/enemy_horizontal.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bumpo1 : MonoBehaviour {
+
+	public float min;
+	public float max;
+
+	// Use this for initialization
+	void Start () {
+
+		min=transform.position.x;
+		max=transform.position.x+6;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		transform.position =new Vector3(Mathf.PingPong(Time.time*2,max-min)+min, transform.position.y, transform.position.z);
+
+	}
+}
---
+++ doughreturn_game/Assets/Scripts/enemy_vertical.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bumpo1 : MonoBehaviour {
+
+	public float min;
+	public float max;
+
+	// Use this for initialization
+	void Start () {
+
+		min=transform.position.y;
+		max=transform.position.y+6;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		transform.position =new Vector3(transform.position.x, Mathf.PingPong(Time.time*2,max-min)+min, transform.position.z);
+
+	}
+}
