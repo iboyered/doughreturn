1ce7554c8e04673c3dbf98cdba7966bc8e13678f
Patrick Fitzgibbon <patrickfitzgibbon@wustl.edu>
2018-04-16 15:18:42 -0500

    working sounds and music, added platform textures but unsuccessful tiling

---
+++ doughreturn_game/Assets/Scripts/MusicScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicScript : MonoBehaviour {
+
+	public AudioClip musicClip;
+	public AudioSource musicSource;
+
+	void Start () {
+		musicSource.clip = musicClip;
+	}
+	
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.M)) {
+			musicSource.Play ();
+		}
+	}
+}
---
+++ doughreturn_game/Assets/Scripts/SoundScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundScript : MonoBehaviour {
+
+	public AudioClip soundClip;
+	public AudioSource soundSource;
+
+	void Start () {
+		soundSource.clip = soundClip;
+	}
+
+	void PlaySound () {
+		soundSource.Play ();
+	}
+}
